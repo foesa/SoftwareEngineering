b58706a66ad4f050d01882e9bea0a71225463baf
Foesa <leguavoen@gmail.com>
2019-10-01 23:19:42 +0100

    Basic LCA code implemented. Need to begin testing in new file. Also need to comment code.

---
+++ LCA.py
@@ -0,0 +1,26 @@
+class Node:
+
+    def __init__(self,key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+def LCAsearch(root, n1, n2):
+    if root is None:
+        return False;
+
+    if root.key == n1.key or root.key == n2.key:
+        return root
+
+    left_lca = LCAsearch(root.left,n1,n2)
+    right_lca = LCAsearch(root.right,n1,n2)
+
+    if left_lca and right_lca:
+        return root
+
+    return left_lca if left_lca != None else right_lca
+
+
+
+
