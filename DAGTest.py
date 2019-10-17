3b7b483b704dd7a22a6665d649addd41d8fe3489
Foesa <leguavoen@gmail.com>
2019-10-17 23:22:38 +0100

    Added Testing for LCADAG

---
+++ DAGTest.py
@@ -0,0 +1,64 @@
+import unittest
+from LcaDAG import Node,LcaDAG
+
+class testing(unittest.TestCase):
+    def test_simple(self):
+        root = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(5)
+        n6 = Node(6)
+        root.succ = [n2, n3, n4, n5]
+        n2.succ = [n4]
+        n2.prev = [root]
+        n3.succ = [n4, n5]
+        n3.prev = [root]
+        n4.succ = [n5]
+        n4.prev = [n2, n3, root]
+        n5.prev = [n3, n4, root]
+        n6.prev = [n4]
+        lca = LcaDAG(root, n4, n5)
+        self.assertEquals(lca,3)
+
+    def test_null(self):
+        root = None
+
+        lca = LcaDAG(root, 4, 5)
+        self.assertEquals(lca,None)
+
+    def test_root(self):
+        root = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        root.succ = [n2, n3]
+
+        lca = LcaDAG(root, root, n3)
+        self.assertEquals(lca, 1)
+        lca = LcaDAG(root, n3, root)
+        self.assertEquals(lca, 1)
+        lca = LcaDAG(root, n2, n2)
+        self.assertEquals(lca, 2)
+
+    def test_no_immediate_lca(self):
+        root = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        n4 = Node(4)
+        n5 = Node(5)
+        n6 = Node(6)
+        root.succ = [n2, n3]
+        n2.succ = [n4]
+        n2.pred = [root]
+        n3.succ = [n4, n5]
+        n3.pred = [root]
+        n4.succ = [n5]
+        n4.pred = [n2]
+        n5.pred = [n3]
+        n6.pred = [n4]
+
+        lca = LcaDAG(root, n4, n5)
+        self.assertEquals(lca, 1)
+
+if __name__ == '__main__':
+    unittest.main()
--- LcaDAG.py
+++ LcaDAG.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, key):
         self.key = key
-        self.pred = []
+        self.prev = []
         self.succ = []
 
 def LcaDAG(root, n1, n2):
@@ -13,14 +13,14 @@ def LcaDAG(root, n1, n2):
         return n1.key
     lca = []
 
-    for i in range(len(n1.pred)):
-        for j in range(len(n2.pred)):
-            if (n1.pred[i].key == n2.pred[j].key):
-                lca.append(n1.pred[i].key)
+    for i in range(len(n1.prev)):
+        for j in range(len(n2.prev)):
+            if (n1.prev[i].key == n2.prev[j].key):
+                lca.append(n1.prev[i].key)
 
     if (lca == []):
         if (n1.key > n2.key):
-            lca.append(LcaDAG(root, n1.pred[0], n2))
+            lca.append(LcaDAG(root, n1.prev[0], n2))
         else:
-            lca.append(LcaDAG(root, n1, n2.pred[0]))
+            lca.append(LcaDAG(root, n1, n2.prev[0]))
     return max(lca)
