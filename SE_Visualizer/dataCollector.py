edf7549ed4270fc1ce43c6b4c875c8a256b74043
foesa <leguavoen@gmail.com>
2019-11-11 17:15:12 +0000

    Can now get commit messages also.

--- SE_Visualizer/dataCollector.py
+++ SE_Visualizer/dataCollector.py
@@ -6,4 +6,4 @@ repo = curUser.get_repo("foesa/PortfolioProjects")
 commits = repo.get_commits()
 for commit in commits:
     stats = commit.stats
-    print(stats.additions,stats.deletions,stats.total)
+    print(stats.additions,stats.deletions,stats.total,commit.commit.message)
