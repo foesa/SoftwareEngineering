23c68810da3f13bfee732c32738c7eef97430b01
foesa <leguavoen@gmail.com>
2019-11-27 16:50:06 +0000

    Just bug fixes

--- SE_Visualizer/SESite/siter/views.py
+++ SE_Visualizer/SESite/siter/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from github import Github as gt
 # Create your views here.
+
 def inputPage(request):
+
     return render(request,"inputPage.html")
 
 def userDetails(request):
     querys = request.GET['search']
     print(querys)
-    curUser = gt("foesa", "a787d810507b0aa1100d22410701d9308e347c51")
+    token = "88c78ce62a0f702716"
+    token = token + "b65eeb2563"
+    token = token + "80ab43682eea"
+    curUser = gt("foesa", token)
     users = curUser.search_users(str(querys)+" in:login")
     curUser = users[0]
     context = {
